84e0a23c66f2458aff79c715f800eb5e524f6797
Nikolas Walker <zerkus10@gmail.com>
2021-12-01 22:55:54 -0600

    Update front_server.js

--- front_server.js
+++ front_server.js
@@ -21,10 +21,21 @@ app.get('/', function(req, res) {
 });
 
 app.get('/edit', function(req, res) {
-    var exampleVar = "Javascript";
-    
+    // create a variable to hold the hero name parsed from the request body
+   // create a variable to hold the username parsed from the request body
+   var username = req.body.username
+   // create a variable to hold ....
+   var password = req.body.password
+ /*I think this is how we could start off the page to edit information
+ I think a variable to get the id might be required but since that is handeled in the python part 
+ it might not be needed here*/
+
+   console.log("email is: " + username);
+   console.log("password is: " + password);
+   console.log("checkedbox checked: " + check); 
+
     // this will render our new example spage 
-    res.render("pages/edit.ejs", {exampleVar: exampleVar});
+    res.render("pages/edit.ejs", {body: req.body});
 });
 
 app.post('/process_form', function(req, res){ //change this for the examples to reflect the hero name being passed into the api
@@ -37,12 +48,23 @@ app.post('/process_form', function(req, res){ //change this for the examples to 
  in dinenrparty so this will probably need to be changed to firstname and lastname I guess.
  
  I am thinking of making this the home page but I am keeping it as process_form so I can track how
- this is supposed to work at the moemnt with the HTML pages*/
+ this is supposed to work at the moment with the HTML pages*/
 
-   let check = 0;
-
-   if (req.body.rememberme == 'on')
-       check = 1;
+ //maybe changing the route to user_edit would be useful fro the python and javascript
+ axios.get('http://127.0.0.1:5000/user' + hero_name) //concatenate the desired hero name from the body to end of the api to get information about them
+ .then((response)=>{
+/* I am not sure about what to do this with part below whichc is why it is still here.
+Something liek this might be needed I know the 
+axios is since this is supposed to use stuff form the python file*/
+     var hero_name = response.data; //saves the resposne to the variable
+     var tagline = "Displaying known identies for selected superhero in Bat-Computer daatabase:";
+     console.log(hero_name); //shows all the files related to the search term in the console
+     res.render('pages/hero_info_retrieved.ejs', 
+     {
+         hero_name: hero_name, 
+         tagline: tagline
+     });
+ });
 
    console.log("email is: " + username);
    console.log("password is: " + password);
@@ -50,19 +72,6 @@ app.post('/process_form', function(req, res){ //change this for the examples to 
 
    res.render('pages/thanks.ejs', {body: req.body})
 
-    console.log("Searching Bat-Computer files for: " + hero_name); //passes the entered name to the console
-    axios.get('http://127.0.0.1:5000/user' + hero_name) //concatenate the desired hero name from the body to end of the api to get information about them
-    .then((response)=>{
-
-        var hero_name = response.data; //saves the resposne to the variable
-        var tagline = "Displaying known identies for selected superhero in Bat-Computer daatabase:";
-        console.log(hero_name); //shows all the files related to the search term in the console
-        res.render('pages/hero_info_retrieved.ejs', 
-        {
-            hero_name: hero_name, 
-            tagline: tagline
-        });
-    });
 })
 
 
