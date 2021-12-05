16ecdad0dd69615b2bcb50aa93fe1d3b541d986e
Nikolas Walker <zerkus10@gmail.com>
2021-12-04 22:52:27 -0600

    Add files via upload

---
+++ Nikolas/dinnerparty.py
@@ -0,0 +1,165 @@
+"""
+CIS 3368 Final Project Sprint 1
+
+Team Members: 
+    Mohammad Qureshi PSID: 1789301
+    Nikolas Walker PSID: 1469899
+"""
+
+import flask
+import random
+import sql
+from flask import jsonify
+from flask import request, make_response
+from sql import create_connection
+from sql import execute_query
+from mysql.connector import Error
+
+
+app = flask.Flask(__name__) #  sets up the application
+app.config["DEBUG"] = True #  allow to show errors in browser
+
+
+@app.route('/user', methods=['POST']) 
+def add_user():
+    request_data = request.get_json()
+    firstname = request_data['firstname']
+    lastname = request_data['lastname']
+    conn = create_connection("cis3368.c9cmn090gmpt.us-east-2.rds.amazonaws.com", "admin", "cis3368database", "cis3368fall21")
+    query = "INSERT INTO FinalProject.usertable (firstname, lastname) VALUES ('"+firstname+"','"+lastname+"')"
+    # runs SQL code to allow data to be inserted into usertable for names
+    execute_query(conn, query)
+    return 'POST REQUEST WORKED'
+
+
+@app.route('/user', methods=['GET'])
+def show_user():
+    # if 'id' in request.args: #only if an id is provided as an argument will it proceed
+    #     id = int(request.args['id'])
+    # else:
+    #     return 'ERROR: No ID provided!'
+    conn = create_connection("cis3368.c9cmn090gmpt.us-east-2.rds.amazonaws.com", "admin", "cis3368database", "cis3368fall21")
+    cursor = conn.cursor(dictionary=True)
+    sql = "SELECT * FROM FinalProject.usertable" # finds the user information for the id passed in
+
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    results = []
+
+    for user in rows:
+        # if user['id'] == id:
+        results.append(user)
+    return jsonify(results) # shows the results of the user id passed in with json formatting after iterating over its data
+
+# gets the id for each set of names and saves them inot a list called results so they can be called and displayed together
+# this allows for ?id=1 and other versions to be entered so the data saved can be displayed using the right key
+
+# maybe rename it to user_edit so it is easier to tell them apart
+@app.route('/user', methods=['PUT']) # this method allows for the users information to be updated
+def update_user():
+    request_data = request.get_json()
+    userid = request_data['id']
+    firstname = request_data['firstname']
+    lastname = request_data['lastname']
+    conn = create_connection("cis3368.c9cmn090gmpt.us-east-2.rds.amazonaws.com", "admin", "cis3368database", "cis3368fall21")
+    query = "UPDATE FinalProject.usertable SET firstname = '{}', lastname = '{}' WHERE id = '{}'".format(firstname, lastname, userid)
+    execute_query(conn, query)
+    return 'POST REQUEST WORKED'
+
+
+@app.route('/user/restaurantlist', methods=['POST']) # requests the data for resturants to be entered in json format
+def add_restaurant():
+    request_data = request.get_json()
+    userid = request_data['user_id']
+    restaurant1 = request_data['restaurant1']
+    restaurant2 = request_data['restaurant2']
+    restaurant3 = request_data['restaurant3']
+    restaurant4 = request_data['restaurant4']
+    restaurant5 = request_data['restaurant5']
+    restaurant6 = request_data['restaurant6']
+    restaurant7 = request_data['restaurant7']
+    restaurant8 = request_data['restaurant8']
+    restaurant9 = request_data['restaurant9']
+    restaurant10 = request_data['restaurant10']
+    conn = create_connection("cis3368.c9cmn090gmpt.us-east-2.rds.amazonaws.com", "admin", "cis3368database", "cis3368fall21")
+    query = "INSERT INTO FinalProject.restauranttable (user_id, restaurant1, restaurant2, restaurant3, restaurant4, restaurant5, restaurant6, restaurant7, restaurant8, restaurant9, restaurant10) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(userid, restaurant1, restaurant2, restaurant3, restaurant4, restaurant5, restaurant6, restaurant7, restaurant8, restaurant9, restaurant10)
+    execute_query(conn, query) # executes the SQL insertion query for the data to be saved into the resturanttable table
+    return 'POST REQUEST WORKED'
+
+#  though long this is establishing the connection for an id to be assigned and the restaurants to be put together for 1 person
+
+
+@app.route('/user/restaurantlist', methods=['GET'])
+def show_restaurant():
+    if 'user_id' in request.args: #  only if an id is provided as an argument, proceed
+        user_id = int(request.args['user_id'])
+    else:
+        return 'ERROR: No ID provided!'
+    conn = create_connection("cis3368.c9cmn090gmpt.us-east-2.rds.amazonaws.com", "admin", "cis3368database", "cis3368fall21")
+    cursor = conn.cursor(dictionary=True)
+    sql = "SELECT * FROM FinalProject.restauranttable where user_id = %s" %(user_id)
+
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    results = []  # list to store all the values form restauranttable by id
+
+    for user in rows:
+        if user['user_id'] == user_id:
+            results.append(user)
+    return jsonify(results)
+# much like the one for user an id is retrieved and stored into the list so it can be dsiplaeyd with the right key
+
+
+@app.route('/update/restaurantlist', methods=['PUT'])
+def update_restaurantlist():
+    request_data = request.get_json()
+    userid = request_data['user_id']
+    restaurant1 = request_data['restaurant1']
+    restaurant2 = request_data['restaurant2']
+    restaurant3 = request_data['restaurant3']
+    restaurant4 = request_data['restaurant4']
+    restaurant5 = request_data['restaurant5']
+    restaurant6 = request_data['restaurant6']
+    restaurant7 = request_data['restaurant7']
+    restaurant8 = request_data['restaurant8']
+    restaurant9 = request_data['restaurant9']
+    restaurant10 = request_data['restaurant10']
+    conn = create_connection("cis3368.c9cmn090gmpt.us-east-2.rds.amazonaws.com", "admin", "cis3368database", "cis3368fall21")
+    query = "UPDATE FinalProject.restauranttable SET id = '{}', restaurant1 = '{}', restaurant2 ='{}', restaurant3 ='{}', restaurant4 ='{}', restaurant5 ='{}', restaurant6 ='{}', restaurant7 ='{}', restaurant8 ='{}', restaurant9 ='{}', restaurant10 ='{}'".format(userid, restaurant1, restaurant2, restaurant3, restaurant4, restaurant5, restaurant6, restaurant7, restaurant8, restaurant9, restaurant10)
+    # runs the SQL command for updating a tables values for resturant names 
+    execute_query(conn, query)
+    return 'POST REQUEST WORKED'
+
+
+@app.route('/randomrestaurant', methods=['GET'])
+def get_randomrestaurant():
+    if 'user_ids' in request.args:
+        user_ids = [int(x.strip()) for x in request.args['user_ids'].split(',')] # add api parameters into a list called 'user_ids' using a for loop. Add commas between each parameter. 
+    else:
+        return "ERROR: No User ID provided!" # return error if no 'user_ids' are provided
+
+    conn = create_connection("cis3368.c9cmn090gmpt.us-east-2.rds.amazonaws.com", "admin", "cis3368database", "cis3368fall21")
+    cursor = conn.cursor(dictionary=True)
+    if len(user_ids) == 1:
+        t = "('{}')".format(user_ids[0]) #if only 1 user is entered into the api, make t equal to the user_id in parenthesis for the SQL statement
+        #t = user_ids[0]
+    else:
+        t = tuple(user_ids) # if more than 1 user is entered, turn the list into a tuple that equals t. This allows us to enter our parameters into the SQL statement with parenthesis.
+
+    sql = "SELECT * FROM FinalProject.restauranttable where user_id IN {}".format(t) # get back a table with only the users entered using the variable t.
+    cursor.execute(sql) # execute the query
+    rows = cursor.fetchall() 
+
+    all_user_restaurants = [] # create an empty list to add all user restaurants
+    restaurant_data = [] # list to remove id
+
+    for r in rows:  # remove id and user_id from the SELECT query list
+        if r != 'id' and r != 'user_id': #loop through each item in rows and only append items in the list that are restaurants.
+         restaurant_data.append(r)
+
+    for restaurants in restaurant_data: # loop through each restaurant in the restaurant_data list
+        all_user_restaurants+= restaurants.values() # add each restaurant value to the empty list created above(all_user_restaurants). This removes the key value from the dictionary in the restaurant_data list.
+    return jsonify({"random_restaurant":random.choice(all_user_restaurants)}) #return one restaurant using random import from the list.
+
+app.run()
+# this is needed for the app route commands to run
---
+++ Nikolas/front_server.js
@@ -0,0 +1,140 @@
+// load the things we need
+var express = require('express');
+var app = express();
+const bodyParser  = require('body-parser');
+
+// required module to make calls to a REST API
+const axios = require('axios');
+
+var selectedID = "";
+app.use(bodyParser.urlencoded());
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+
+
+/* the home page at the moemnt shoudl bring up a form to enter a name and password 
+but it shouldn't be to difficult to change the name stuff for our purposes.
+For now this is kind of a filler*/
+app.get('/', function(req, res) {
+    var exampleVar = "Javascript";
+    res.render("pages/account.ejs", {exampleVar: exampleVar});
+});
+
+
+// named it app.put for the time being
+app.put('/edit', function(req, res) {
+    // create a variable to hold the hero name parsed from the request body
+   // create a variable to hold the username parsed from the request body
+   var username = req.body.firstname
+   // create a variable to hold ....
+   var password = req.body.lastname
+ /*I think this is how we could start off the page to edit information
+ I think a variable to get the id might be required but since that is handeled in the python part 
+ it might not be needed here*/
+
+   console.log("firstname is: " + firstname);
+   console.log("lastname is: " + lastname);
+   console.log("checkedbox checked: " + check); 
+
+    // this will render our new example spage 
+    res.render("pages/edit.ejs", {body: req.body});
+});
+
+
+
+//More palcehodler ideas
+//edit_resturant
+//pass in the user info that we want to update
+app.put('/edit', function(req, res) {
+    axios.get('http://127.0.0.1:5000/update/restaurantlist' + hero_name) //concatenate the desired hero name from the body to end of the api to get information about them
+ .then((response)=>{
+ var hero_name = response.data; //saves the resposne to the variable
+ var tagline = "Displaying known identies for selected superhero in Bat-Computer daatabase:";
+ console.log(hero_name);
+    res.render('pages/choose');
+    });
+   // create a variable to hold the username parsed from the request body
+   var username = req.body.username
+   // create a variable to hold ....
+   var password = req.body.password
+ /*I think this is how we could start off the page to edit information
+ I think a variable to get the id might be required but since that is handeled in the python part 
+ it might not be needed here*/
+
+   console.log("email is: " + username);
+   console.log("password is: " + password);
+   console.log("checkedbox checked: " + check); 
+
+    // this will render our new example spage 
+    res.render("pages/edit.ejs", {body: req.body});
+});
+
+
+
+
+app.post('/process_form', function(req, res){ //change this for the examples to reflect the hero name being passed into the api
+   // create a variable to hold the hero name parsed from the request body
+   // create a variable to hold the username parsed from the request body
+   var username = req.body.username
+   // create a variable to hold ....
+   var password = req.body.password
+ /*I think it is meant to be setup like this but I am not entirely sure if that macthes what we have 
+ in dinenrparty so this will probably need to be changed to firstname and lastname I guess.
+ 
+ I am thinking of making this the home page but I am keeping it as process_form so I can track how
+ this is supposed to work at the moment with the HTML pages*/
+
+ //maybe changing the route to user_edit would be useful fro the python and javascript
+ axios.get('http://127.0.0.1:5000/user' + hero_name) //concatenate the desired hero name from the body to end of the api to get information about them
+ .then((response)=>{
+/* I am not sure about what to do this with part below whichc is why it is still here.
+Something liek this might be needed I know the 
+axios is since this is supposed to use stuff form the python file*/
+     var hero_name = response.data; //saves the resposne to the variable
+     var tagline = "Displaying known identies for selected superhero in Bat-Computer daatabase:";
+     console.log(hero_name); //shows all the files related to the search term in the console
+     res.render('pages/choose.ejs', 
+     {
+         hero_name: hero_name, 
+         tagline: tagline
+     });
+ });
+
+   console.log("email is: " + username);
+   console.log("password is: " + password);
+   console.log("checkedbox checked: " + check); 
+
+   res.render('pages/thanks.ejs', {body: req.body})
+
+})
+
+
+
+
+/* the final path at the moemnt that shhould use the random selection and dispaly it
+right  now this is also just a placeholder */
+app.get('/choose', function(req, res) {
+  var restaurant = req.params.restaurant
+    axios.get('http://127.0.0.1:5000/randomrestaurant' + restaurant) //concatenate the desired hero name from the body to end of the api to get information about them
+    .then((response)=>{
+    var restaurant = response.data; //saves the resposne to the variable
+    var tagline = "Displaying the randomly selected resturant:";
+    console.log(restaurant);
+    
+        res.render('pages/choose',
+        {
+          restaurant: restaurant,
+          tagline: tagline
+        })
+      });
+});
+
+
+
+
+app.listen(8080);
+console.log('8080 is the magic port');
