1b6c982457e61255328d11a837942bd66c7fe6a6
Nikolas Walker <zerkus10@gmail.com>
2021-12-04 23:29:54 -0600

    Add files via upload

--- Nikolas/front_server.js
+++ Nikolas/front_server.js
@@ -12,129 +12,110 @@ app.use(bodyParser.urlencoded());
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
 
 
 
-/* the home page at the moemnt shoudl bring up a form to enter a name and password 
-but it shouldn't be to difficult to change the name stuff for our purposes.
-For now this is kind of a filler*/
+// Home page 
 app.get('/', function(req, res) {
-    var exampleVar = "Javascript";
-    res.render("pages/account.ejs", {exampleVar: exampleVar});
-});
+    var tagline = "Created by Houston Consulting Solutions";
 
-
-// named it app.put for the time being
-app.put('/edit', function(req, res) {
-    // create a variable to hold the hero name parsed from the request body
-   // create a variable to hold the username parsed from the request body
-   var username = req.body.firstname
-   // create a variable to hold ....
-   var password = req.body.lastname
- /*I think this is how we could start off the page to edit information
- I think a variable to get the id might be required but since that is handeled in the python part 
- it might not be needed here*/
-
-   console.log("firstname is: " + firstname);
-   console.log("lastname is: " + lastname);
-   console.log("checkedbox checked: " + check); 
-
-    // this will render our new example spage 
-    res.render("pages/edit.ejs", {body: req.body});
-});
-
-
-
-//More palcehodler ideas
-//edit_resturant
-//pass in the user info that we want to update
-app.put('/edit', function(req, res) {
-    axios.get('http://127.0.0.1:5000/update/restaurantlist' + hero_name) //concatenate the desired hero name from the body to end of the api to get information about them
- .then((response)=>{
- var hero_name = response.data; //saves the resposne to the variable
- var tagline = "Displaying known identies for selected superhero in Bat-Computer daatabase:";
- console.log(hero_name);
-    res.render('pages/choose');
+    res.render('pages/home', {
+        tagline: tagline
     });
-   // create a variable to hold the username parsed from the request body
-   var username = req.body.username
-   // create a variable to hold ....
-   var password = req.body.password
- /*I think this is how we could start off the page to edit information
- I think a variable to get the id might be required but since that is handeled in the python part 
- it might not be needed here*/
-
-   console.log("email is: " + username);
-   console.log("password is: " + password);
-   console.log("checkedbox checked: " + check); 
-
-    // this will render our new example spage 
-    res.render("pages/edit.ejs", {body: req.body});
 });
 
 
 
+// adduser page
+app.get('/adduser_form', function(req, res) {
 
-app.post('/process_form', function(req, res){ //change this for the examples to reflect the hero name being passed into the api
-   // create a variable to hold the hero name parsed from the request body
-   // create a variable to hold the username parsed from the request body
-   var username = req.body.username
-   // create a variable to hold ....
-   var password = req.body.password
- /*I think it is meant to be setup like this but I am not entirely sure if that macthes what we have 
- in dinenrparty so this will probably need to be changed to firstname and lastname I guess.
- 
- I am thinking of making this the home page but I am keeping it as process_form so I can track how
- this is supposed to work at the moment with the HTML pages*/
-
- //maybe changing the route to user_edit would be useful fro the python and javascript
- axios.get('http://127.0.0.1:5000/user' + hero_name) //concatenate the desired hero name from the body to end of the api to get information about them
- .then((response)=>{
-/* I am not sure about what to do this with part below whichc is why it is still here.
-Something liek this might be needed I know the 
-axios is since this is supposed to use stuff form the python file*/
-     var hero_name = response.data; //saves the resposne to the variable
-     var tagline = "Displaying known identies for selected superhero in Bat-Computer daatabase:";
-     console.log(hero_name); //shows all the files related to the search term in the console
-     res.render('pages/choose.ejs', 
-     {
-         hero_name: hero_name, 
-         tagline: tagline
-     });
- });
-
-   console.log("email is: " + username);
-   console.log("password is: " + password);
-   console.log("checkedbox checked: " + check); 
-
-   res.render('pages/thanks.ejs', {body: req.body})
-
-})
-
-
-
-
-/* the final path at the moemnt that shhould use the random selection and dispaly it
-right  now this is also just a placeholder */
-app.get('/choose', function(req, res) {
-  var restaurant = req.params.restaurant
-    axios.get('http://127.0.0.1:5000/randomrestaurant' + restaurant) //concatenate the desired hero name from the body to end of the api to get information about them
+    axios.get(`http://127.0.0.1:5000//user`)
     .then((response)=>{
-    var restaurant = response.data; //saves the resposne to the variable
-    var tagline = "Displaying the randomly selected resturant:";
-    console.log(restaurant);
     
-        res.render('pages/choose',
-        {
-          restaurant: restaurant,
-          tagline: tagline
-        })
-      });
+    var firstname = req.body.firstname
+    var lasttname = req.body.lasttname 
+    var tagline = response.data
+    axios.post(`http://127.0.0.1:5000//user` + firstname + lasttname)
+    console.log(firstname, lasttname, tagline);
+        // use res.render to load up an ejs view file
+    res.render('pages/add_user.ejs', {
+        firstname: firstname,
+        lasttname: lasttname,
+        tagline: tagline
+    });
+}); 
+});
+
+
+app.get('/useradded', function(req, res) {
+    axios.get(`http://127.0.0.1:5000//user`)
+    .then((response)=>{
+    
+        var firstname = response.data;
+        var tagline = "Here is the data coming from my own API";
+        console.log(firstname);
+         // use res.render to load up an ejs view file
+        res.render('pages/home.ejs', {
+            firstname: firstname,
+            tagline: tagline
+    });
+}); 
 });
 
 
 
+// edit user page
+app.get('/edituser_form', function(req, res) {
+    var userid = req.body.userid
+    var firstname = req.body.firstname
+    var lastname = req.body.lasttname
+    console.log("Searching for: " + userid)
+    axios.all([axios.get('http://127.0.0.1:5000/user'),
+    axios.get('http://127.0.0.1:5000/user?user_ids=' + userid),
+    axios.put('http://127.0.0.1:5000/user?user_ids=' + userid)]) 
+    .then(axios.spread((firstResponse, secondResponse, thirdResponse) =>{
+    
+        var userid = secondResponse.data
+        var firstname = firstResponse.data
+        var lastname = thirdResponse.data
+        
+        // userid: userid,
+        // firstname: firstname,
+        // lastname: lasttname
+        console.log("Search found: " + userid)
+         // use res.render to load up an ejs view file
+        res.render('pages/edit_user', {
+            body: req.body,
+            userid: userid,
+            firstname: firstname,
+            lastname: lastname
+    }); 
+}))
+});
+
+app.get('/viewuser_form', function(req, res) {
+    axios.get('http://127.0.0.1:5000/user')
+    .then((response)=>{
+        var id = response.data;
+        console.log(id);
+        res.render('pages/viewusers.ejs',{
+            id: id
+
+        });
+    });
+});
+
+app.get('/restaurant_form', function(req, res) {
+    axios.get('http://127.0.0.1:5000/user')
+    .then((response)=>{
+        var id = response.data;
+        console.log(id);
+        res.render('pages/viewusers.ejs',{
+            id: id
+
+        });
+    });
+});
 
 app.listen(8080);
 console.log('8080 is the magic port');
